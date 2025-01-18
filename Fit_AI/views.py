630eb36103f55d1f9e389e3509027a07a5337efd
Ateeksh Seth <ateeksh.seth@gmail.com>
2025-01-19 01:20:23 +0530

    adding openai modules

--- Fit_AI/views.py
+++ Fit_AI/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
+from openai import OpenAI
+from django.http import JsonResponse
 
 # Create your views here.
+@csrf_exempt
+def ask(request):
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        prompt = body.get("prompt", "")  
+        client = OpenAI()
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=prompt
+        )
+        return JsonResponse({'access': completion})
+
+
