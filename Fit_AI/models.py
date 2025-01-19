2084c17f4527e581db456a1d102f76a7df8a6d13
Ateeksh Seth <ateeksh.seth@gmail.com>
2025-01-19 17:18:09 +0530

    Database changes

--- Fit_AI/models.py
+++ Fit_AI/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class UserAccount(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
@@ -8,20 +7,32 @@ class UserAccount(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class FoodItem(models.Model):
     name = models.CharField(max_length=255)
     calories = models.IntegerField()
     protein = models.FloatField()
-    usage = models.BooleanField(default=False)
+    eaten = models.BooleanField(default=False)
+
     def __str__(self):
         return self.name
+class Excersise(models.Model):
+    name = models.CharField(max_length=255)
+    Reps = models.DecimalField(decimal_places=0,max_digits=2) 
+    Sets =  models.DecimalField(decimal_places=2,max_digits=5) 
+    Time = models.PositiveIntegerField()  
+    eaten = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+    
 class Fitness(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255) 
-    food_items = models.ManyToManyField(FoodItem, related_name='diets')
+    diet = models.ManyToManyField(FoodItem, related_name='diets')
+    excersise = models.ManyToManyField(FoodItem, related_name='excersise')
+
     def __str__(self):
         return self.name
     
@@ -29,7 +40,32 @@ class UserProfile(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     weight = models.DecimalField(decimal_places=2,max_digits=5) 
+    weight_goal = models.DecimalField(decimal_places=2,max_digits=5) 
     height = models.DecimalField(decimal_places=2,max_digits=5) 
+    Medical_history = models.TextField()
+    Additional_information = models.TextField()
     def __str__(self):
         return self.name
     
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255) 
+    steps =  models.DecimalField(decimal_places=0,max_digits=5) 
+    sleep = models.DecimalField(decimal_places=0,max_digits=2) 
+    calories = models.DecimalField(decimal_places=0,max_digits=5) 
+    weight = models.DecimalField(decimal_places=2,max_digits=5) 
+    protien = models.DecimalField(decimal_places=2,max_digits=5) 
+    def __str__(self):
+        return f"{self.user.name} - {self.date}"
+
+class UserHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='history')
+    date = models.DateField(auto_now_add=True)  
+    steps = models.DecimalField(decimal_places=0, max_digits=5) 
+    sleep = models.DecimalField(decimal_places=0, max_digits=2) 
+    calories = models.DecimalField(decimal_places=0, max_digits=5) 
+    weight = models.DecimalField(decimal_places=2, max_digits=5) 
+    protien = models.DecimalField(decimal_places=2, max_digits=5)
+    
+    def __str__(self):
+        return f"{self.user.name} - {self.date}"
