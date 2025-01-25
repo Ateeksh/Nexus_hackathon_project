da31526737ee6f20e822a1da218ac44da858ea1a
Ateeksh Seth <ateeksh.seth@gmail.com>
2025-01-25 13:46:51 +0530

    new changes

--- Fitness_app/settings.py
+++ Fitness_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qxt=1x%(p_8!v+=x9#!)t!$xr#ia3fdf8gks38@j9i8ar5#1im
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fitterme.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
